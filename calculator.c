a1819d42adc33b82133bd31b2238884c0162bece
Farida <Farecho>
2024-02-13 14:06:43 +0300

    Farida 's  Calc

---
+++ calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    double a, b, result; 
+    char operator;
+
+    printf("Enter First Number: \n"); 
+    scanf("%lf", &a);
+
+    printf("Choose (+ or -): \n"); 
+    scanf(" %c", &operator); // Notice the space before %c
+
+    printf("Enter Second Number: \n"); 
+    scanf("%lf", &b);
+
+    if(operator == '+') {
+        result = a + b; 
+        printf("The answer is: %lf\n", result);
+    } else if(operator == '-') {
+        result = a - b;  
+        printf("The asssnswer is: %lf\n", result);
+    }
+
+    return 0; 
+}
